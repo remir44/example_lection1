90c8e28424c0dd0df7f956a9e886d3feb6f60b89
Remir <tengubird800@gmail.com>
2022-09-22 14:06:32 +0300

    added example010 method array

---
+++ example010_methodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 1, 12, 31, 41, 15, 66, 17, 8, 16 };
+
+int n = array.Length;
+int find = 16;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
