1de7545af34ab7334b34984a2e28b078772014d1
Remir <tengubird800@gmail.com>
2022-09-27 13:27:16 +0300

    added example012 methods

---
+++ example012_methods/Program.cs
@@ -0,0 +1,27 @@
+﻿// int Method() // вывести нынешний год
+// {
+//     return DateTime.Now.Year;
+// }
+// int year = Method();
+// Console.WriteLine(year);
+
+// string Method2(int count, string text) // вывести несколько склеенных слов/строк
+// {
+//     string result = String.Empty;
+//     for (int i = 0; i < count; i++)
+//     {
+//         result+=text;
+//     }
+//     return result;
+// }
+// string res = Method2(4,"qwerty");
+// Console.WriteLine(res);
+
+// for (int i = 2; i <= 9; i++) // вывести таблицу умножения
+// {
+//     for (int j = 2; j <= 9; j++)
+//     {
+//         Console.WriteLine($"{i} * {j} = {i * j}");
+//     }
+//     Console.WriteLine();
+// }
